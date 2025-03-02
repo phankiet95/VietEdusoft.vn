b34439b7342a079cf44e8fba1253ca8293f3fda5
Kiet Phan <kietphan54k2@gmail.com>
2025-03-02 18:32:59 +0900

    update fix bug crossword

--- OlympiaCrossWord/CustomAsset/js/main.js
+++ OlympiaCrossWord/CustomAsset/js/main.js
@@ -206,41 +206,14 @@ $(document).ready(function () {
     generateHtml();
   });
 
-  // $(document).on('click', '#nav_exportGame', function () {
-  //   console.log('Start download Data');
-  //   // Lưu game Intro
-  //   //saveIntro();
-  //   let dataSaved = JSON.stringify({ data: window.slideData });
-  //   download(`${window.slideData.setting.gameinfo.name}_${getTimeForFileName()}.VietEduSoft`, dataSaved);
-  
-  // });
-
   $(document).on('click', '#nav_exportGame', function () {
     console.log('Start download Data');
-    //window.isLoading(true);
     // Lưu game Intro
     //saveIntro();
-    let dataSaved = JSON.stringify({ data: window.questionList, setting });
-    //download(`${window.setting.gameinfo.name}_${getTimeForFileName()}.VietEduSoft`, dataSaved);
-    downloadPlainText(`${window.setting.gameinfo.name}_${getTimeForFileName()}.VietEduSoft`, dataSaved);
-  
-    setTimeout(() => {
-      window.isLoading(false);
-    }, 1000)
+    let dataSaved = JSON.stringify({ data: window.slideData });
+    download(`${window.slideData.setting.gameinfo.name}_${getTimeForFileName()}.VietEduSoft`, dataSaved);
   
   });
-
-  function downloadPlainText(filename, text) {
-    console.log('New Download function');
-    const blob = new Blob([text], { type: 'text/plain' });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(link.href);
-  }
 
   // kiet new add
   $(document).on('change', '#nav_importGame', function () {
